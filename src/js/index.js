0ea6bec872f832fd35cb4389f4d489a10754ca9c
= <=>
2023-11-21 21:13:16 -0300

    Changes to be committed: 	modified:   index.html 	modified:   src/js/index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,20 @@ const ids = [
     "listaPedidos", "addnewIng", "deletbnt", "addpditobnt",
     "selectTypePratos", "adicionarItenBtn", "selectPratos",
     "removerIten", "confirmarPedidoBtn", "removerIngredientes",
-    "formularioPrato"
+    "formularioPrato", "pedidoModalLabelalter", "alterarbnt",
+    "selectPratosAlter", "selectTypePratosAlter", "addnwitenAlter",
+    "selectPratosAlter", "removerItenAlter"
 ];
 const dom = new DomMaster(ids, new Log());
 const req = new Request("http://localhost:8080/");
 let pedidosList = [];
+let pedidosListAlter = [];
+let idsRem = [];
+let pedidosAlterList = null;
+let addListAlter = {
+    id: null,
+    elementList: pedidosListAlter
+}
 
 window.addEventListener("DOMContentLoaded", async () => {
     await getAllPedidos();
@@ -29,7 +38,13 @@ dom.addAction('click', "addpditobnt", async () => {
 dom.addAction('change', "selectTypePratos", async () => {
     var element = dom.get("selectTypePratos")
     var selectedValue = element.options[element.selectedIndex].value;
-    await getPratosByType(selectedValue);
+    await getPratosByType(selectedValue, "selectPratos");
+});
+
+dom.addAction('change', "selectTypePratosAlter", async () => {
+    var element = dom.get("selectTypePratosAlter")
+    var selectedValue = element.options[element.selectedIndex].value;
+    await getPratosByType(selectedValue, "selectPratosAlter");
 });
 
 dom.addAction('click', "adicionarItenBtn", () => {
@@ -47,12 +62,37 @@ dom.addAction('click', "adicionarItenBtn", () => {
 
     pedidosList.push(dados)
 
-    renderItensAdicionados(pedidosList);
+    renderItensAdicionados(pedidosList, "itensAdicionados");
 });
+
+dom.addAction('click', "addnwitenAlter", () => {
+
+    var element = dom.get("selectTypePratosAlter")
+    var selectedValue = element.options[element.selectedIndex].value;
+
+    var elementprato = dom.get("selectPratosAlter")
+    var selectedValueprato = elementprato.options[elementprato.selectedIndex].value;
+    var nomep = selectedValueprato.match(/^(\d+)\/(.+)$/);
+
+    const dados = {
+        type: selectedValue,
+        name: nomep[2],
+        id: nomep[1]
+    }
+
+    pedidosListAlter.push(dados)
+    renderItensAdicionados(pedidosListAlter, "itensAdicionadosAlter");
+
+})
 
 dom.addAction('click', "removerIten", () => {
     pedidosList.pop();
-    renderItensAdicionados(pedidosList);
+    renderItensAdicionados(pedidosList, "itensAdicionados");
+});
+
+dom.addAction('click', "removerItenAlter", () => {
+    pedidosListAlter.pop();
+    renderItensAdicionados(pedidosListAlter, "itensAdicionadosAlter");
 });
 
 dom.addAction('click', "confirmarPedidoBtn", () => {
@@ -100,6 +140,27 @@ dom.addAction('submit', "formularioPrato", async (event) => {
 
 })
 
+dom.addAction("click", "alterarbnt", async () => {
+    await updateWithAdd();
+    try {
+        isValidObject(pedidosAlterList);
+        req.clear()
+        const r = await req.url("alter/comanda")
+            .post()
+            .json()
+            .cors()
+            .body(pedidosAlterList)
+            .origenReq()
+        .send()
+
+        pedidosAlterList = null;
+        idsRem = [];
+    } catch (error) {
+        
+    }
+    await ReRender();
+    $('#alterModal').modal('hide');
+})
 
 async function PratosType() {
     const data = await req.url("info/getTypes/prato")
@@ -110,6 +171,7 @@ async function PratosType() {
     const types = data.types;
     const selectElement = document.getElementById("tipoPrato");
     const selectElementNewP = document.getElementById("selectTypePratos");
+    const selectElement1 = document.getElementById("selectTypePratosAlter");
 
     selectElement.innerHTML = ""
     types.forEach(type => {
@@ -125,6 +187,15 @@ async function PratosType() {
         option.value = type;
         option.text = type;
         selectElementNewP.add(option);
+    });
+    
+
+    selectElement1.innerHTML = ""
+    types.forEach(type => {
+        const option = document.createElement("option");
+        option.value = type;
+        option.text = type;
+        selectElement1.add(option);
     });
 
     req.clear()
@@ -186,11 +257,17 @@ async function renderinfoPrato() {
 
 async function updatePedido(id) {
     req.clear()
+    idsRem = []
 
     const data = await req.url("info/get/comanda/byid/"+id)
+        .get()
+        .cors()
+    .send();
 
+    dom.get("pedidoModalLabelalter").innerHTML = "Alterar pedido: " + data.id;
+    addListAlter.id = data.id
+    addItemToAlterList(data);
 
-    console.log(id);
     $('#alterModal').modal('show');
 }
 
@@ -260,9 +337,9 @@ function deletePrato(id) {
     $('#deleteModal').modal('hide');
 }
 
-function renderItensAdicionados(object) {
+function renderItensAdicionados(object, id) {
 
-    var itensAdicionadosDiv = document.getElementById("itensAdicionados");
+    var itensAdicionadosDiv = document.getElementById(id);
     itensAdicionadosDiv.innerHTML = "";
     object.forEach(element => {
         var type = element.type
@@ -273,7 +350,7 @@ function renderItensAdicionados(object) {
     });
 }
 
-async function getPratosByType(type) {
+async function getPratosByType(type, id) {
     const pratoReq = new Request("http://localhost:8080/");
     const data = await pratoReq.url("info/get/pratos/bytype/" + type)
         .get()
@@ -281,15 +358,15 @@ async function getPratosByType(type) {
         .send();
 
     const pratos = data.pratos;
-    renderpratosSelect(pratos)
+    renderpratosSelect(pratos, id)
 }
 
-function renderpratosSelect(list) {
+function renderpratosSelect(list, id) {
     if (!Array.isArray(list)) {
         throw new Error("A lista não é um array!");
     }
 
-    var selectElement = document.getElementById("selectPratos");
+    var selectElement = document.getElementById(id);
     selectElement.innerHTML = '';
     list.forEach(element => {
         var option = document.createElement("option");
@@ -297,6 +374,7 @@ function renderpratosSelect(list) {
         option.text = element.nome;
         selectElement.add(option);
     });
+
 }
 
 async function newPedido(pedidos) {
@@ -329,9 +407,7 @@ async function getAllPedidos() {
     const data = await pratoReq.url("info/get/pedidos")
         .get()
         .cors()
-        .send();
-
-    pratoReq.clear()
+    .send();
 
     const pedidosList = data.listaPedidos;
     render(pedidosList);
@@ -347,6 +423,82 @@ function ocultarErro(id) {
     var erroSection = document.getElementById(id);
     erroSection.innerHTML = "";
     erroSection.style.display = "none";
+}
+
+function addItemToAlterList(items) {
+    var lista = document.getElementById("listaItensAlterar");
+    lista.innerHTML = ""; 
+    let cout = 1;
+
+    items.pratoinfos.forEach(function (itemText) {
+        var novoItem = document.createElement("li");
+        novoItem.className = "list-group-item d-flex justify-content-between align-items-center";
+
+        var textoItem = document.createTextNode("pedido n°: "+cout+" -> "+itemText.nome);
+
+        var botaoExcluir = document.createElement("span");
+        botaoExcluir.className = "badge badge-danger badge-pill";
+        botaoExcluir.textContent = "X";
+        botaoExcluir.onclick = function () {
+            removeElementAlter(itemText, items)
+        };
+
+        novoItem.appendChild(textoItem);
+        novoItem.appendChild(botaoExcluir);
+
+        lista.appendChild(novoItem);
+        cout++;
+    });
+}
+
+function removeElementAlter(target, list){
+    let array = list.pratoinfos;
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if(element.id === target.id){
+            array.splice(index, 1);
+            idsRem.push(element.id);
+        }
+        
+    }
+
+    list.pratoinfos = array;
+    pedidosAlterList = list;
+    pedidosAlterList["removes"] =  idsRem;
+    addItemToAlterList(pedidosAlterList)
+
+}
+
+function isValidObject(object){
+    if(object === null || object == undefined){
+        throw new Error("objeto invalido");
+    }
+}
+
+async function ReRender(){
+    const pratoReq = new Request("http://localhost:8080/");
+    const data = await pratoReq.url("info/get/pedidos")
+        .get()
+        .cors()
+    .send();
+
+    const pedidosListr = data.listaPedidos;
+    render(pedidosListr)
+}
+
+async function updateWithAdd(){
+    const r = await req.url("alter/comanda/add")
+        .post()
+        .json()
+        .cors()
+        .body(addListAlter)
+        .origenReq()
+    .send();
+
+    addListAlter = {
+        id: null,
+        elementList: pedidosListAlter
+    }
 }
 
 
